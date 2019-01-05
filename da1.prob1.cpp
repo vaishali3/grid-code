4e447d0d897a389e601b8298ea63500bb4f90c73
vaishali3 <36641757+vaishali3@users.noreply.github.com>
2019-01-05 16:02:11 +0530

    Add files via upload

---
+++ da1.prob1.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+int m,n;
+	vector< vector<char> >x;
+	vector<char>y;
+bool res(string str,int a,int b,int s)
+{
+	if(a<0 || b<0 || a>m-1 || b>n-1)
+	  return false;
+	  if(x[a][b]==str[s] && s==(str.length()-1))
+	  return true;
+	if(x[a][b]==str[s])
+	{
+		if(res(str,a,b-1,s+1))
+		return true;
+		if(res(str,a,b+1,s+1))
+		return true;
+		if(res(str,a-1,b,s+1))
+		return true;
+		if(res(str,a+1,b,s+1))
+		return true;
+		return false;
+	}
+	return false;
+}
+int main()
+{
+	string str;
+	cout<<"enter size"<<endl;
+	cin>>m>>n;
+	cout<<"enter grid"<<endl;
+	char k;
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cin>>k;
+			y.push_back(k);
+		}
+		x.push_back(y);
+				y.clear();
+	}
+	cout<<"enter string"<<endl;
+	cin>>str;
+	bool c=0;
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(res(str,i,j,0))
+			{
+				c=1;
+				break;
+			}	
+		}
+		if(c==1)
+		break;
+	}
+	if(c)
+	cout<<endl<<"true";
+	else
+	cout<<endl<<"false";
+	return 0;
+}
